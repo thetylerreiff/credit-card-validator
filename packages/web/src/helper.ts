8ad8eadf349e3d1f4de7a1db46c619e2e04f17bc
Tyler Reiff <tyler.reiff@postscript.io>
2023-11-16 14:14:33 -0500

    Add client validation errror handeling

--- packages/web/src/helper.ts
+++ packages/web/src/helper.ts
@@ -4,20 +4,26 @@ const API_URL = process.env.REACT_APP_API_URL || `http://localhost:4000`;
 
 export const validate = async (values: FormValues) => {
     const { cardNumber } = values;
-    const response = await fetch(
-        `${API_URL}/validate`,
-        {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            method: 'POST',
-            body: JSON.stringify({ cardNumber }),
+    try {
+
+        const response = await fetch(
+            `${API_URL}/validate`,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: JSON.stringify({ cardNumber }),
+            }
+        );
+        const data = await response.json();
+        if (!data.isValid) {
+            return { cardNumber: data.message };
+        } else {
+            return {};
         }
-    );
-    const data = await response.json();
-    if (!data.isValid) {
-        return { cardNumber: data.message };
-    } else {
-        return {};
+    }
+    catch {
+        return { cardNumber: 'Error validating card number' };
     }
 }
